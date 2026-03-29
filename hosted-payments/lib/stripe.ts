874ff2dbcee8e54581dae5a6e44d4851c1309fd9
Steve Smith <stephen@cncpowder.com>
2026-03-29 09:49:11 -0500

    Update hosted payments Stripe API version

--- hosted-payments/lib/stripe.ts
+++ hosted-payments/lib/stripe.ts
@@ -7,7 +7,7 @@ let stripe: Stripe | null = null;
 export function getStripe() {
   if (!stripe) {
     stripe = new Stripe(getEnv("STRIPE_SECRET_KEY"), {
-      apiVersion: "2025-02-24.acacia"
+      apiVersion: "2025-08-27.basil"
     });
   }
   return stripe;
